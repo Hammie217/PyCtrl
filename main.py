d6aab750c37a32caab3e766ad076d968e203a055
Hammie217 <hammie217@gmail.com>
2019-09-29 18:33:00 +0100

    Added color

--- main.py
+++ main.py
@@ -1,11 +1,60 @@
 def cursorUp(val):
-    if((val)>0 and (isinstance(val, int))):
-        print(u"\u001b[" + str(val) + "A",end='')
-        return 0
+    if(isinstance(val, int)):
+        if((val)>0):
+            print(u"\u001b[" + str(val) + "A",end='')
+            return 0
+        return -1
+    return -1
+def cursorDown(val):
+    if(isinstance(val, int)):
+        if((val)>0):
+            print(u"\u001b[" + str(val) + "B",end='')
+            return 0
+        return -1
+    return -1
+
+def cursorRight(val):
+    if(isinstance(val, int)):
+        if((val)>0):
+            print(u"\u001b["+str(val)+"C",end='')
+            return 0
+        return -1
     return -1
 
 def cursorLeft(val):
-    if(val>0):
-        print(u"\u001b["+str(val)+"D")
+    if(isinstance(val, int)):
+        if((val)>0):
+            print(u"\u001b["+str(val)+"D",end='')
+            return 0
+        return -1
+    return -1
+
+def setColor(val):
+    if(isinstance(val, str)):
+        val=val.upper()
+        if(val=="BLACK"):
+            print("\u001b[30m",end="")
+        elif(val=="RED"):
+            print("\u001b[31m",end="")
+        elif(val=="GREEN"):
+            print("\u001b[32m",end="")
+        elif(val=="YELLOW"):
+            print("\u001b[33m",end="")
+        elif(val=="BLUE"):
+            print("\u001b[34m",end="")
+        elif(val=="MAGENTA"):
+            print("\u001b[35m",end="")
+        elif(val=="CYAN"):
+            print("\u001b[36m",end="")
+        elif(val=="WHITE"):
+            print("\u001b[37m",end="")
+        elif(val=="RESET"):
+            print("\u001b[0m",end="")
+        else:
+            return -1
         return 0
     return -1
+
+
+
+
--- test.py
+++ test.py
@@ -1,11 +1,70 @@
 import pytest
 from main import *
 
+def testCursorUp():
+    assert cursorUp(0) == -1
+    assert cursorUp(-1) == -1
+    assert cursorUp(-100) == -1
+    assert cursorUp(0.1) == -1
+    assert cursorUp("N") == -1
+    assert cursorUp('f') == -1
+    assert cursorUp([1,2]) == -1
+    assert cursorUp(1) == 0
+    assert cursorUp(5) == 0
+def testCursorDown():
+    assert cursorLeft(0) == -1
+    assert cursorLeft(-1) == -1
+    assert cursorLeft(-100) == -1
+    assert cursorLeft(0.1) == -1
+    assert cursorLeft("N") == -1
+    assert cursorLeft('f') == -1
+    assert cursorLeft([1,2]) == -1
+    assert cursorLeft(1) == 0
+    assert cursorLeft(5) == 0
+def testCursorRight():
+    assert cursorUp(0) == -1
+    assert cursorUp(-1) == -1
+    assert cursorUp(-100) == -1
+    assert cursorUp(0.1) == -1
+    assert cursorUp("N") == -1
+    assert cursorUp('f') == -1
+    assert cursorUp([1,2]) == -1
+    assert cursorUp(1) == 0
+    assert cursorUp(5) == 0
+def testCursorLeft():
+    assert cursorLeft(0) == -1
+    assert cursorLeft(-1) == -1
+    assert cursorLeft(-100) == -1
+    assert cursorLeft(0.1) == -1
+    assert cursorLeft("N") == -1
+    assert cursorLeft('f') == -1
+    assert cursorLeft([1,2]) == -1
+    assert cursorLeft(1) == 0
+    assert cursorLeft(5) == 0
+
+def testSetColor():
+    assert setColor("Red")==0
+    print("Red")
+    assert setColor("black")==0
+    print("black")
+    assert setColor("WhItE")==0
+    print("WhItE")
+    assert setColor("Green")==0
+    print("Green")
+    assert setColor("RESET")==0
+    print("RESET")
+    assert setColor("Gren")==-1
+    print("Gren")
+    assert setColor("unknown")==-1
+    print("UNKNOWN")
+    assert setColor(6)==-1
+    print("6")
+    assert setColor("R")==-1
+    print("!\"red!\"")
+
 print("Initiating test \n\n\n\n\n\n")
-assert cursorUp(0) == -1
-assert cursorUp(-1) == -1
-assert cursorUp(-100) == -1
-assert cursorUp(0.1) == -1
-assert cursorUp(1) == 0
-assert cursorUp(5) == 0
+testCursorUp()
+testCursorLeft()
+testSetColor()
+
 print("Completed Test")
